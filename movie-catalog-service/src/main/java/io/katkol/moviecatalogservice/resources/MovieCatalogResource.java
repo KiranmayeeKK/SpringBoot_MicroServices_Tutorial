8f34541ceb6462ecb9ac7d8c35d140186b004cb2
KiranmayeeKK <katyayani.kolluru@gmail.com>
2021-01-24 12:45:18 +0100

    used WebClient to make REST API calls

--- movie-catalog-service/src/main/java/io/katkol/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/katkol/moviecatalogservice/resources/MovieCatalogResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.katkol.moviecatalogservice.models.CatalogItem;
 import io.katkol.moviecatalogservice.models.Movie;
@@ -21,6 +22,9 @@ public class MovieCatalogResource {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId")String UserId){
@@ -41,8 +45,16 @@ public class MovieCatalogResource {
 		
 		//To call the movie Info service, an API call has to be made using REST Template
 		return ratings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-		return new CatalogItem(movie.getName(),"Test",rating.getRating());
+			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = webClientBuilder.build() //returns a client
+			.get() //says that it is going to be REST get function
+			.uri("http://localhost:8082/movies/" + rating.getMovieId()) //	url where request to be made
+			.retrieve() //go to the fetch
+			.bodyToMono(Movie.class) //says whatever body you get back, convert it to movie instance. It is a reactive way
+			//of promise that says you are going to get an object back but at later point of time 
+			.block(); //As the getCatalog is synchronous call, all its code has to be executed before return statement. Eventhough this is an async call, it has to wait until it gets the response 
+			
+			return new CatalogItem(movie.getName(),"Test",rating.getRating());
 		}).collect(Collectors.toList());
 
 		
