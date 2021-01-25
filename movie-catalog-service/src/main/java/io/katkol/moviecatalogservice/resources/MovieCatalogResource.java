d8e9c61261f481c155678cfe799f9befdc022c5c
KiranmayeeKK <katyayani.kolluru@gmail.com>
2021-01-25 18:59:17 +0100

    Added Eureka Service discovery
    A eureka discovery server is created and the 3 applications are converted to eureka clients and the movie catalog service is updated to remove hardcoded urls like
    http://localost:8083/ratings and it is replaced with http://ratings-data-service/ratings which looks like url but contains the application name and it is resolved by
    contacting eureka discovery server which is done by the RestTemplate. It is annotated with LoadBalanced. Refer to video https://www.youtube.com/watch?v=GxLjcOE35oA&list=PLqq-6Pq4lTTZSKAFG6aCDVDP86Qx4lNas&index=21&ab_channel=JavaBrains

--- movie-catalog-service/src/main/java/io/katkol/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/katkol/moviecatalogservice/resources/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,23 +23,21 @@ import io.katkol.moviecatalogservice.models.UserRating;
 public class MovieCatalogResource {
 	
 	@Autowired
+	@LoadBalanced
 	private RestTemplate restTemplate;
-	
-	@Autowired
-	private WebClient.Builder webClientBuilder;
 	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId")String userId){
 		
 		//get all rated movie IDs
 
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 		
 		//For each movie ID, call movie info service and get details
 		
 		//To call the movie Info service, an API call has to be made using REST Template
 		return ratings.getUserRating().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
 			return new CatalogItem(movie.getName(),"Test",rating.getRating());
 		}).collect(Collectors.toList());
